a29d515e9cca338e8cc8bf512b4d66933ca78a65
yuxiaoguang <757771107@qq.com>
2020-08-12 14:11:22 +0800

    完成马赛克

---
+++ Filter_23469Demo/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Filter_23469Demo
+//
+//  Created by 鲸鱼集团技术部 on 2020/8/10.
+//  Copyright © 2020 com.sanqi.net. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+@end
---
+++ Filter_23469Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Filter_23469Demo
+//
+//  Created by 鲸鱼集团技术部 on 2020/8/10.
+//  Copyright © 2020 com.sanqi.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
